4e37f57a1eabdc04edcf36b4b62b6a92cd5a3777
Vladyslav Dryk <vladyslav.dryk@gmail.com>
2025-01-05 23:15:36 +0200

    create gateway module

--- gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/com/example/gateway/GatewayApplicationTests.java
@@ -1,11 +1,10 @@
-package com.example.user_service;
+package com.example.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootTest
-class UserServiceApplicationTests {
+class GatewayApplicationTests {
 
     @Test
     void contextLoads() {
